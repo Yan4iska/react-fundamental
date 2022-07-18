383b5fbe3652c159b6c2d766fe1cdbb7cfbc5530
yan4iska <matveev.git@gmail.com>
2022-07-18 09:33:57 +0900

    React instead of summer practice.

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fuga dolorum tempore odio neque veniam dolores blanditiis possimus iste quis ea voluptatum aliquid sapiente magnam nisi, voluptas similique quo minus?
+        </h1>
+    );
+};
+
+export default About;
